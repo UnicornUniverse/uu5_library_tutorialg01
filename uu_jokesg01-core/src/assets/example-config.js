7e58ccca97a615ace48700445ee5701acd77022c
Martin Farkaš <martin.farkas@unicorn.com>
2022-09-26 15:26:26 +0200

    Switch uuDcc for uuEcc in brick demo file

--- uu_jokesg01-core/src/assets/example-config.js
+++ uu_jokesg01-core/src/assets/example-config.js
@@ -1,7 +1,7 @@
 const imports = {
   react: "https://cdn.plus4u.net/libs/react/17.0.1/react.min.js",
   "react-dom": "https://cdn.plus4u.net/libs/react-dom/17.0.1/react-dom.min.js",
-  "create-react-class": "https://cdn.plus4u.net/libs/create-react-class/15.6.3/create-react-class.js",
+  "create-react-class": "https://cdn.plus4u.net/libs/create-react-class/15.6.3/create-react-class.min.js",
   "prop-types": "https://cdn.plus4u.net/libs/prop-types/15.7.2/prop-types.min.js",
 
   uu5g04: "https://cdn.plus4u.net/uu-uu5g04/1.0.0/uu5g04.js",
@@ -10,11 +10,12 @@ const imports = {
   "uu5g04-hooks": "https://cdn.plus4u.net/uu-uu5g04/1.0.0/uu5g04-hooks.js",
   "uu5g04-block-layout": "https://cdn.plus4u.net/uu-uu5g04/1.0.0/uu5g04-block-layout.js",
 
-  uu5g05: "https://cdn.plus4u.net/uu-uu5g05/1.0.0/uu5g05.js",
+  uu5g05: "https://cdn.plus4u.net/uu-uu5g05/1.0.0/uu5g05.min.js",
   "uu5g05-editing": "https://cdn.plus4u.net/uu-uu5g05/1.0.0/uu5g05-editing.min.js",
-  "uu5g05-elements": "https://cdn.plus4u.net/uu-uu5g05/1.0.0/uu5g05-elements.js",
-  "uu5g05-forms": "https://cdn.plus4u.net/uu-uu5g05/1.0.0/uu5g05-forms.js",
+  "uu5g05-elements": "https://cdn.plus4u.net/uu-uu5g05/1.0.0/uu5g05-elements.min.js",
+  "uu5g05-forms": "https://cdn.plus4u.net/uu-uu5g05/1.0.0/uu5g05-forms.min.js",
   "uu5g05-bricks-support": "https://cdn.plus4u.net/uu-uu5g05/1.0.0/uu5g05-bricks-support.min.js",
+  "uu5g05-dev": "https://cdn.plus4u.net/uu-uu5g05/1.0.0/uu5g05-dev.min.js",
   uu_i18ng01: "https://cdn.plus4u.net/uu-i18ng01/1.0.0/uu_i18ng01.min.js",
   uu5stringg01: "https://cdn.plus4u.net/uu-uu5stringg01/1.0.0/uu5stringg01.min.js",
 
@@ -32,12 +33,13 @@ const imports = {
   uu_territoryg01: "https://cdn.plus4u.net/uu-territoryg01/2.0.0/uu_territoryg01.min.js",
   "uu_territoryg01-bricks": "https://cdn.plus4u.net/uu-territoryg01/2.0.0/uu_territoryg01-bricks.min.js",
 
-  uu5codekitg01: "https://cdn.plus4u.net/uu-uu5codekitg01/1.0.0/uu5codekitg01.min.js",
-  "uu5codekitg01-markdown": "https://cdn.plus4u.net/uu-uu5codekitg01/1.0.0/uu5codekitg01-markdown.min.js",
   uu5richtextg01: "https://cdn.plus4u.net/uu-uu5richtextg01/1.0.0/uu5richtextg01.min.js",
   immutable: "https://cdn.plus4u.net/libs/immutable/3.8.2/immutable.min.js",
-  uu_dynamiccomponentcontentg02:
-    "https://cdn.plus4u.net/uu-dynamiccomponentcontentg02/3.0.0/uu_dynamiccomponentcontentg02.min.js",
+  uu_editablecomponentcontentg03:
+    "https://cdn.plus4u.net/uu-editablecomponentcontentg03/3.0.0/uu_editablecomponentcontentg03.min.js",
+  "uu_applibraryregistryg01-bricks":
+    "https://cdn.plus4u.net/uu-applibraryregistryg01/1.0.0/uu_applibraryregistryg01-bricks.min.js",
+  uu5tilesg02: "https://cdn.plus4u.net/uu-uu5tilesg02/1.0.0/uu5tilesg02.min.js",
 
   uu_jokesg01: "https://cdn.plus4u.net/uu-jokesg01/0.0.0/uu_jokesg01.min.js",
   "uu_jokesg01-core": "https://cdn.plus4u.net/uu-jokesg01/0.0.0/uu_jokesg01-core.min.js",
